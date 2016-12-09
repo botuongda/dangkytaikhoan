7ed52a6018edd94e6482d5f3a5161f02920092e8
botuongda <b0tu0ngda@yahoo.com.vn>
2016-12-09 14:37:12 +0700

    Add files via upload

--- dangky.js
+++ dangky.js
@@ -28,24 +28,105 @@ for (var i=1967; i <= 2016; i++){
 $("#year").html(srt);
 
 
-// Lấy thông tin	
-		var array=["#tendk","#ngaydk","#emaildk","#dienthoaidk","#gioitinhdk"]
-		var x=decodeURIComponent(window.location.href);
-		var i=x.indexOf("ten");
-			x=x.slice(i,x.length);
-		var	str=x.split("&");
-		var chuoi="";
-		var ten=str[0].split("=")[1]
-			ten=ten.split("+");
-		for (var i = 0; i<ten.length; i++) {
-			chuoi += " " + ten[i]
+// Kiểm tra thông tin
+$("#register").on("submit", function (){
+	var iValid=true;
+	if($("#name").val().trim() == "")
+	{
+		$("#name").next('span').text('Bạn chưa nhập tên')
+		iValid = false;
+	}
+	else if(locdau().match(/^\s*[a-zA-Z,\s]+\s*$/) == null)
+
+		{
+		$("#name").next('span').text('Bạn nhập sai tên')
+		iValid = false;
 		}
-		$(array[0]).html("Tên đăng ký là : " + chuoi);	
-		$(array[1]).html("Ngày " + str[1].split("=")[1] + " tháng " + str[2].split("=")[1] + " năm sinh " + str[3].split("=")[1]);		
-		$(array[2]).html("Địa chỉ email là: " + str[4].split("=")[1]);
-		$(array[3]).html("Số điện thoại : " + str[5].split("=")[1]);
-		$(array[4]).html("Giới tính : " + str[6].split("=")[1]);
+
+	else {$("#name").next('span').text('')}
+	// if($("#pass").val().trim() == "")
+	// {
+	// 	$("#pass").next('span').text('Bạn chưa nhập mật khẩu')
+	// 	iValid = false;
+	// }
+	// else if($("#pass").val().match(/^(?=[^\d_].*?\d)\w(\w|[!@#$%]){7,20}/) == null)
+
+	// 	{
+	// 	$("#pass").next('span').text('Mật khẩu phải có 8 ký tự và kèm 1 số')
+	// 	iValid = false;
+	// 	}
+
+	// else {$("#pass").next('span').text('')}
+
+	if($("#day").val() == "Ngày")
+	{
+		$("#year").next('span').text('Bạn chưa chọn ngày sinh')
+		iValid = false;
+	}
+	else if($("#month").val() == "Tháng")
+		{
+		$("#year").next('span').text('Bạn chưa nhập tháng')
+		iValid = false;
+		}
+	else if($("#year").val() == "Năm sinh")
+		{
+		$("#year").next('span').text('Bạn chưa chọn năm')
+		iValid = false;
+		}
+	else {$("#year").next('span').text('')}
+
+		//kiểm tra email
+	if($("#email").val().trim() == "")
+	{
+		$("#email").next('span').text('Bạn chưa nhập email')
+		iValid = false;
+	}
+	else if($("#email").val().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/) == null)
+	{
+		$("#email").next('span').text('Bạn nhập sai địa chỉ email')
+		iValid = false;
+	}
+	else {$("#email").next('span').text('')}
+
+		if($("#facebook").val().trim() == "")
+	{
+		$("#facebook").next('span').text('Bạn chưa nhập địa chỉ facebook')
+		iValid = false;
+	}
+	else {$("#facebook").next('span').text('')}
+		if($("#phone").val().trim() == "")
+	{
+	//chưa sử lý $("#phone").next('span').add("<i class="fa fa-plus-square" aria-hidden="true"></i>"
+ 		$("#phone").next('span').text('Bạn chưa nhập số điện thoại')
+		iValid = false;
+	}
+	else if($("#phone").val().match(/^[\\(]{0,1}([0-9]){3}[\\)]{0,1}[ ]?([^0-1]){1}([0-9]){2}[ ]?[-]?[ ]?([0-9]){4}[ ]*((x){0,1}([0-9]){1,5}){0,1}$/) == null)
+	{
+		$("#phone").next('span').text('Bạn nhập sai số điện thoại')
+		iValid = false;
+	}
+	else {$("#phone").next('span').text('')}
+
+	
+	if($("#nam").prop('checked')==false && $("#nu").prop('checked')==false)
+		{
+		$("#nu").nextAll('span').text('Bạn chưa chọn giới tính')
+		iValid = false;
+		}
+	else {$("#nu").nextAll('span').text('')}
+	return iValid;
+})
 
 
-// "ten=bùi+đức+anh&day=05&month=11&year=1992&email=anhbuiduc893@gmail.com
-// &phone=0972893666&gender=nam"
+function locdau(){
+var str = $("#name").val();
+        str = str.toLowerCase();
+        str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, "a");
+        str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, "e");
+        str = str.replace(/ì|í|ị|ỉ|ĩ/g, "i");
+        str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, "o");
+        str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, "u");
+        str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, "y");
+        str = str.replace(/đ/g, "d");
+        return str;
+}
---
+++ dangkythanhcong.js
@@ -0,0 +1,22 @@
+// Lấy thông tin	
+		var array=["#tendk","#ngaydk","#emaildk","#facebookdk","#dienthoaidk","#gioitinhdk"]
+		var x=decodeURIComponent(window.location.href);
+		var i=x.indexOf("ten");
+			x=x.slice(i,x.length);
+		var	str=x.split("&");
+		var chuoi="";
+		var ten=str[0].split("=")[1]
+			ten=ten.split("+");
+		for (var i = 0; i<ten.length; i++) {
+			chuoi += " " + ten[i]
+		}
+		$(array[0]).html("Tên đăng ký là : " + chuoi);	
+		$(array[1]).html("Ngày " + str[1].split("=")[1] + " tháng " + str[2].split("=")[1] + " năm sinh " + str[3].split("=")[1]);		
+		$(array[2]).html("Địa chỉ email là: " + str[4].split("=")[1]);
+		$(array[3]).html("Địa chỉ facebook là: " + str[5].split("=")[1]);
+		$(array[4]).html("Số điện thoại : " + str[6].split("=")[1]);
+		$(array[5]).html("Giới tính : " + str[7].split("=")[1]);
+
+
+// "ten=bùi+đức+anh&day=05&month=11&year=1992&email=anhbuiduc893@gmail.com
+// &phone=0972893666&gender=nam"
